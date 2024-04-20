ab6af0c735ff28dbd3c7840274a50c54c5d462f7
rouhis <eeturouhiainen1@gmail.com>
2024-04-20 18:25:41 +0300

    asd

--- src/api/controllers/authController.ts
+++ src/api/controllers/authController.ts
@@ -17,7 +17,7 @@ const login = async (
   next: NextFunction
 ) => {
   try {
-    //ASDDDD
+    //A
     const {username, password} = req.body;
     const user = await userModel.findOne({email: username});
     if (!user) {
